17bc9271ae90d18a09c2ea70256795987a214c01
Jonathan Rocha <johnferreirar@gmail.com>
2022-08-06 17:46:47 -0300

    test: ensure submit button is disabled when fetching address

--- tests/components/form/form.spec.tsx
+++ tests/components/form/form.spec.tsx
@@ -5,20 +5,33 @@ jest.mock("@apollo/client", () => ({
   useReactiveVar: jest.fn(),
 }));
 
+const searchMock = jest.fn();
+const isFetchingAddressMock = jest.fn();
+
 jest.mock("@/hooks/useAddressSearch", () => ({
   useAddressSearch: jest.fn(() => ({
-    search: jest.fn(),
+    search: searchMock,
   })),
-  isFetchingAddress: false,
+  isFetchingAddress: isFetchingAddressMock,
 }));
 
 describe("Form Component", () => {
   it("Should render without crashing", async () => {
+    isFetchingAddressMock.mockReturnValue(false);
+
     render(<Form />);
 
     expect(screen.getByText("Search for your city")).toBeInTheDocument();
     expect(screen.getByTestId("countryCode")).toBeInTheDocument();
     expect(screen.getByTestId("zipCode")).toBeInTheDocument();
     expect(screen.getByTestId("lookup-submit")).toBeInTheDocument();
+  });
+
+  it(`Should disable the submit button when the form is loading`, async () => {
+    isFetchingAddressMock.mockReturnValue(true);
+
+    render(<Form />);
+
+    expect(screen.getByTestId("lookup-submit")).toBeDisabled();
   });
 });
