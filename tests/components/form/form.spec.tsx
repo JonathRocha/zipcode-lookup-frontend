6fefe4407fa3453e8cd304fa471b98ee6b239f86
Jonathan Rocha <johnferreirar@gmail.com>
2022-08-06 18:35:15 -0300

    test: ensure form is reset when the form is submitted

--- tests/components/form/form.spec.tsx
+++ tests/components/form/form.spec.tsx
@@ -1,5 +1,5 @@
 import { Form } from "@/components/form";
-import { fireEvent, render, screen } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 
 const searchStub = jest.fn();
 const isFetchingAddressStub = jest.fn();
@@ -57,5 +57,19 @@ describe("Form Component", () => {
     fireEvent.click(screen.getByTestId("lookup-submit"));
 
     expect(searchStub).toHaveBeenCalledWith("12345", "US");
+  });
+
+  it("Should reset the form after submit", async () => {
+    render(<Form />);
+
+    fireEvent.change(screen.getByTestId("zipCode"), { target: { value: "12345" } });
+    fireEvent.change(screen.getByTestId("countryCode"), { target: { value: "BR" } });
+    fireEvent.click(screen.getByTestId("lookup-submit"));
+
+    await waitFor(() => {
+      expect(screen.getByTestId("zipCode")?.getAttribute("value")).toBe("");
+      expect(screen.getByTestId("countryCode")?.getAttribute("value")).toBe(null);
+      expect(screen.getByTestId("lookup-submit")).toBeDisabled();
+    });
   });
 });
